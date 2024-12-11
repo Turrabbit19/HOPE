9ee1fc671f4b4a3b8b3933e72b8248b021796f0b
digorry19 <125790557+digorry19@users.noreply.github.com>
2024-12-11 21:39:53 +0700

    Update UI student dashboard

--- fe/src/components/layout/layoutStudent/aside/page.jsx
+++ fe/src/components/layout/layoutStudent/aside/page.jsx
@@ -118,83 +118,23 @@ const AsideClient = () => {
             Đăng kí lịch học
           </Link>
 
-          {/* <a
-                        href="#"
-                        className="flex items-center p-2 text text-gray-700 hover:bg-gray-200"
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="#007AFF"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            className="w-5 h-5 mr-2"
-                        >
-                            <rect width="20" height="8" x="2" y="14" rx="2"></rect>
-                            <path d="M6.01 18H6"></path>
-                            <path d="M10.01 18H10"></path>
-                            <path d="M15 10v4"></path>
-                            <path d="M17.84 7.17a4 4 0 0 0-5.66 0"></path>
-                            <path d="M20.66 4.34a8 8 0 0 0-11.31 0"></path>
-                        </svg>
-                        RTL
-                    </a>
-                    <a
-                        href="#"
-                        className="flex items-center p-2 text text-gray-700 hover:bg-gray-200"
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="#5856D6"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            className="w-5 h-5 mr-2"
-                        >
-                            <path
-                                d="M21 8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16Z"
-                            ></path>
-                            <path d="m3.3 7 8.7 5 8.7-5"></path>
-                            <path d="M12 22V12"></path>
-                        </svg>
-                        Box
-                    </a>
-                    <a
-                        href="#"
-                        className="flex items-center p-2 text text-gray-700 hover:bg-gray-200"
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="#00B5CC"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            className="w-5 h-5 mr-2"
-                        >
-                            <circle cx="12" cy="12" r="4"></circle>
-                            <path d="M12 4h.01"></path>
-                            <path d="M20 12h.01"></path>
-                            <path d="M12 20h.01"></path>
-                            <path d="M4 12h.01"></path>
-                            <path d="M17.657 6.343h.01"></path>
-                            <path d="M17.657 17.657h.01"></path>
-                            <path d="M6.343 17.657h.01"></path>
-                            <path d="M6.343 6.343h.01"></path>
-                        </svg>
-                        Float
-                    </a> */}
+          <Link to="#" className="group flex items-center p-3 rounded-lg hover:bg-orange-100 transition duration-300">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6 mr-3 text-orange-500 transition-transform transform group-hover:translate-x-1"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M5.5 8.5 9 12l-3.5 3.5L2 12l3.5-3.5Z"></path>
+                <path d="m12 2 3.5 3.5L12 9 8.5 5.5 12 2Z"></path>
+                <path d="M18.5 8.5 22 12l-3.5 3.5L15 12l3.5-3.5Z"></path>
+                <path d="m12 15 3.5 3.5L12 22l-3.5-3.5L12 15Z"></path>
+              </svg>
+              <span className="text-gray-800 font-medium group-hover:text-orange-600">
+                Đăng kí chuyên nghành
+              </span>
+            </Link>
         </nav>
       </div>
       <div>
